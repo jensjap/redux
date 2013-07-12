89cfdad9fe33a038e2af9f99a3fa56b4af7fe06e
Jens Jap <jensjap@gmail.com>
2013-07-11 20:26:32 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SRDRRedux::Application.config.secret_key_base = '5692c51bc4c053f057c417bf8b8d77c5dfab7b7afb5eeaf22fb5cab94954f8ab744aa9569032330641b18bbf59f8d549aaa5a690066e6c24d2d47af0c6f4d96a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SRDRRedux::Application.config.session_store :cookie_store, key: '_SRDR_redux_session'
