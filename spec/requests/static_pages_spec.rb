ae3bb9e00b489e2f9c47b0926b2e51aa5a69114d
Jens Jap <jensjap@gmail.com>
2013-07-14 15:02:08 -0400

    Adds a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
-    let(:base_title) { "Systematic Review Data Repository" }
+    let(:base_title) { "SRDR - Systematic Review Data Repository" }
 
     describe "Home page" do
 
@@ -12,7 +12,12 @@ describe "Static pages" do
 
         it "should have the title 'Home'" do
             visit '/static_pages/home'
-            expect(page).to have_title("#{base_title} | Home")
+            expect(page).to have_title("#{base_title}")
+        end
+
+        it "should not have a custom page title" do
+            visit '/static_pages/home'
+            expect(page).not_to have_title("| Home")
         end
     end
 
